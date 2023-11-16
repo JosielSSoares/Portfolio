3f249e6a973945ab51f01cd9ca701095e10351cf
josiel <josielsoares_2@hotmail.com>
2023-11-16 02:26:31 -0400

    Atualização de Código Em andamento.

--- JavaScript/informacoesPessoais.js
+++ JavaScript/informacoesPessoais.js
@@ -3,6 +3,16 @@ const infoContent = document.getElementById('info-content');
 const isDragging = { value: false };
 const initialPosition = { x: 0, y: 0 };
 
+document.addEventListener('click', (e) => {
+  const isClickInsideInfoContent = infoContent.contains(e.target);
+  const isClickInsidePlusButton = plusButton.contains(e.target);
+
+  if (!isClickInsideInfoContent && !isClickInsidePlusButton) {
+    // Clique fora da caixa e fora do botão '+', então fecha a caixa.
+    infoContent.style.display = 'none';
+  }
+});
+
 function toggleInfo() {
   if (infoContent.style.display === 'block') {
     infoContent.style.display = 'none';
@@ -36,6 +46,28 @@ document.addEventListener('mouseup', () => {
 infoContent.style.position = 'absolute';
 infoContent.style.right = '250px';  // Posição inicial x
 infoContent.style.top = '50px';    // Posição inicial y
+
+document.addEventListener('DOMContentLoaded', function () {
+  const collapsibles = document.querySelectorAll('.collapsible');
+
+  collapsibles.forEach(function (collapsible) {
+    collapsible.addEventListener('click', function () {
+      const content = this.nextElementSibling;
+
+      if (content.style.display === 'block') {
+        content.style.display = 'none';
+        this.querySelector('.fa-caret-down').style.transform = 'rotate(0deg)';
+      } else {
+        content.style.display = 'block';
+        this.querySelector('.fa-caret-down').style.transform = 'rotate(180deg)';
+      }
+    });
+  });
+});
+
+
+
+
 
 
 
